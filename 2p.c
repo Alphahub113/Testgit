dd32ddae5ac62a30fd7084c059f4ddd80015a7e5
Alphahub113 <akhilkumarpakki2002@gmail.com>
2024-07-12 14:56:09 +0530

    2nd basic file

---
+++ 2p.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum=0;
+	printf("Enter two numbers : ");
+	scanf("%d %d", &a,&b);
+
+	sum = a + b;
+
+	printf("Sum of 2 numbers is %d\n", sum);
+	return 0;
+}
